893224d58ea8ff72b6d9d680aeb728ee4a53a737
Geoffreystats <wawerug45@gmail.com>
2025-05-13 17:58:54 +0300

    hey

---
+++ script.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function() {
+  // Example interactivity: Change the background color when clicking on the header
+  const header = document.querySelector("header");
+  header.addEventListener("click", function() {
+    document.body.style.backgroundColor = "#f0f0f0";
+  });
+});
